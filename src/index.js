4a49622bafb35a64496ad1eb6dded06652f0051a
Jaime Rogstad <jaimerogstad@gmail.com>
2022-06-22 21:49:56 -0400

    First commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// To do list
+// Set a valid end date
+// Calculate the time remaining
+// Convert the time to a usable format
+// Output the clock data as a reusable object
+// Display the clock on the page, and stop the clock when it reaches zero
+
